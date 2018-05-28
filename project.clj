25fc88dcb067d2e8ff70093bcdc1e0d0613273de
Islon Scherer <islon.scherer@rewe-digital.com>
2018-05-28 15:05:38 +0200

    create a release and improved readme

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject pure-clj/lein-template "0.1.0-SNAPSHOT"
+(defproject pure-clj/lein-template "0.1.0"
   :description "leiningen `new` template to generate
  pure-clj (purescript Clojure backend) projects."
-  :url "http://example.com/FIXME"
+  :url "https://github.com/stackoverflow/pureclj-lein-template"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
