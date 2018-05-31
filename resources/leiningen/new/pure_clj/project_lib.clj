8c28629ac9053b5682b09607dce08a48d71a6609
Islon Scherer <islon.scherer@gmail.com>
2018-05-31 11:29:29 +0200

    removed purescript src files from lein

--- resources/leiningen/new/pure_clj/project_lib.clj
+++ resources/leiningen/new/pure_clj/project_lib.clj
@@ -4,7 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.9.0"]]
-  :source-paths ["src" "src-clj" "src-gen"]
+  :source-paths ["src-clj" "src-gen"]
   :plugins [[lein-shell "0.5.0"]]
   :repl-options {:init-ns {{namespace}}}
   :aliases {"pursclj" ["shell" "pursclj" "compile"
